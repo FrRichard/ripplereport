90a56aede79ba3b665553e5af359f3eb725f1356
francois.richard12@gmail.com <francois.richard12@gmail.com>
2015-05-22 13:37:22 +0200

    modif app.jade script /js/app.js

--- gulpfile.js
+++ gulpfile.js
@@ -21,18 +21,6 @@ gulp.task("build", ["clean", "sass", "build:prod"]);
 gulp.task("doc", ["jsdoc"]);
 
 
-gulp.task("webpack", function(callback) {
-    // run webpack
-    webpack({
-        // configuration
-    }, function(err, stats) {
-        if(err) throw new gutil.PluginError("webpack", err);
-        gutil.log("[webpack]", stats.toString({
-            // output options
-        }));
-        callback();
-    });
-});
 
 gulp.task('dev-demon', function(cb) {
     nodemon({
