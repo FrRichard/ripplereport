7910c10011be28d0c83cbf2daef63e0ef4cf4518
francois.richard12@gmail.com <francois.richard12@gmail.com>
2015-06-29 09:54:55 +0200

    payment tracking suite

--- client/src/application/views/paymenttracking/PaymentTrackingView.js
+++ client/src/application/views/paymenttracking/PaymentTrackingView.js
@@ -5,16 +5,20 @@ var PaymentStore = require('AccountTransactionsStore');
 var Paymenttracking = React.createClass({
 
 	getInitialState: function() {
-		return null;
+		var id = "address0"; 
+		return  {
+			id:id,
+			address:"",
+			addressList:[]
+		};
 	},
 
 	componentDidMount: function() {
-		PaymentStore.addChangeListener("address1", this._onPaymentUpdate);
+		PaymentStore.addAnyChangeListener("change", this._onPaymentUpdate);
 	},
 
 
-	render: function() {	
-		console.log(this.props.depth);		
+	render: function() {		
 		// var userInput =  <span> <button type=button onClick={this.startTracking}> Start Tracking !</button></span>;
 		return (
 			<div>
@@ -31,20 +35,61 @@ var Paymenttracking = React.createClass({
 		var address = $('.searchinput').val();
 		var account = {
 			address:address,
-			id:"addess1"
+			id:this.state.id
 		};
+		this.count = 0;
+		var addressList = this.state.addressList;
+		addressList.push(address);
+		this.setState({
+			address: address,
+			addressList: addressList
+		});
+		console.log(this.state);
 		Actions.rippleaccounttransactions([account]);
 	},
 
 	_onPaymentUpdate: function(payload) {
-		var top10 = payload
-		for(i = 1; i = this.props.depth; i++) {
 
+		if(this.count < 10) {	
+			var payload = PaymentStore.getSpecific(this.state.id);
+			var i = 0;
+			var address = payload[this.state.id].summary.top10['USD'].sent[i].counterparty;
+			// console.log("compare address_ INWHILE addresstop10",address, "this.state.address:", this.state.address);
+
+			console.log("checkaddreslist_result",this.checkAddressList(address));
+
+			while(address == this.checkAddressList(address) ) {
+				// console.log("compare address_ INWHILE",address, this.state.address);
+				i++;
+				var address = payload[this.state.id].summary.top10['USD'].sent[i].counterparty;
+			}
+			// console.log("address new",address, "addressList", this.state.addressList);
+			this.count++;
+			var account = {
+				address: address,
+				id: "address"+ this.count
+			};
+			Actions.rippleaccounttransactions([account]);
 		}
+		// 	// payload:payload
+		var addressList = this.state.addressList;
+		addressList.push(address);
 
+		console.log(this.state);
 		this.setState({
-			payload:payload
+			id: "address" + this.count,
+			address: address,
+			addressList: addressList
 		});
+	},
+
+	checkAddressList: function(address) {
+		console.log(address, this.state.addressList);
+		var res = _.find(this.state.addresslist, function(al) {
+			return address == al;
+		});
+
+		return res;
 	}
 
 
